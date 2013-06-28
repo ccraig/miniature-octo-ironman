06310f1b7277565364d23e943046e0ce7e276a7f
Charles Craig <ccraig13@gmail.com>
2013-06-27 22:03:05 -0400

    Added Person class

--- main.java
+++ main.java
@@ -3,7 +3,8 @@ class helloGithub {
     public static void main(String  args[]) {
 
         System.out.println("Hello Github");
-
+        Person p = new Person("Omar");
+        System.out.printf("Hello, my name is %s", p.name);
     }
 
 }
